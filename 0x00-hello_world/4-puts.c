79cae10b39af327e43f2248010d48371a3006e8f
Kelvinkiv <kelvinkiv14@yahoo.com>
2023-03-16 13:55:51 -0700

    Hello puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+/**
+ * main - C progaram with puts fucntion
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+Return(0);
+}
