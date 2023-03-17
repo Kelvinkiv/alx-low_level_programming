5a0a0c681b70df5e0501886526c3c8a94b0146ff
Kelvinkiv <kelvinkiv14@yahoo.com>
2023-03-16 19:10:36 -0700

    hello put

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,7 +2,7 @@
 
 
 /**
- * main - prints "programming is like building a multilingual puzzle."
+ * main - prints "programming is like building a multilingual puzzle.
  *
  * Return: 0 (Success)
  */
