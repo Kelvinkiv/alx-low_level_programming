92f56cff8ff180123fede2c4a5a8b1f3245d42f3
Kelvinkiv <kelvinkiv14@yahoo.com>
2023-03-16 23:39:22 -0700

    hello,printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - A C program using printf fucnction
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,");
+	return (0);
+}
