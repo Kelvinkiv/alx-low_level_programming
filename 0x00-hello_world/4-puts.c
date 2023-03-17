70c2c594b219cae066c48a52aff9cb41f74b368e
Kelvinkiv <kelvinkiv14@yahoo.com>
2023-03-16 18:20:39 -0700

    hello,puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,13 @@
 #include <stdio.h>
+
+
 /**
- * main - C progaram with puts fucntion
- *
- * Return: 0
+ * main - prints ""Programming is like building a multilingual puzzle."
+ * Return:0.
  */
+
 int main(void)
+
 {
 puts("\"Programming is like building a multilingual puzzle");
 Return(0);
