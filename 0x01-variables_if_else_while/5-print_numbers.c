126c30089a85448f5da98d89792a585d2ee2e684
Kelvinkiv <kelvinkiv14@yahoo.com>
2023-03-19 09:00:45 -0700

    5-print_numbers.c

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - A program for digit numbers of base 10
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+	int d;
+
+	for (d = 0; d < 10; d++)
+	{
+		printf("%d", d);
+		printf("\n");
+	}
+	return (0);
+}
