17e2e5bbf0412b6762809f7232ea6bfa772b970f
Kelvinkiv <kelvinkiv14@yahoo.com>
2023-03-19 04:49:22 -0700

    alphabet game

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - A rogram tha prints alphabet in lowecase
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+	char c;
+
+	for (c = 'a'; c <= 'z'; c++)
+	putchar(c);
+	putchar('\n');
+	return (0);
+}
