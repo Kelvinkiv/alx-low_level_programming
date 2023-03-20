7a1862485f53bedeefa945d129b75758b42fd5d0
Kelvinkiv <kelvinkiv14@yahoo.com>
2023-03-20 00:02:44 -0700

    task10

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -7,16 +7,16 @@ int main(void)
 {
 	int i, j;
 
-	for (i = 0; i < 10; i++)
+	for (i = '0'; i < '9'; i++)
 	{
-	for (j = i + 1; j < 10; j++)
+	for (j = i + 1; j <= '9'; j++)
 	{
 	if (i != j)
 	{
-	putchar((i) + '0');
-	putchar((j) + '0');
+	putchar(i);
+	putchar(j);
 	if (i == '8' && j == '9')
-		continue;
+	continue;
 	putchar(',');
 	putchar(' ');
 	}
