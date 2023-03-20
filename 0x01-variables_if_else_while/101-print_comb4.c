42a9a029edbe7bcc55397b6dd48350a93ed48711
Kelvinkiv <kelvinkiv14@yahoo.com>
2023-03-20 00:29:11 -0700

    three digit combination

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+/**
+ * main - A program that prints combinations of three digit numbers
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+	int i, j, k;
+
+	for (i = '0'; i < '9'; i++)
+	{
+	for (j = i + 1; j <= '9'; j++)
+	{
+	for (k = j + 1; k <= '9'; k++)
+	{
+	if (i != j && j != k)
+	{
+		putchar(i);
+		putchar(j);
+		putchar(k);
+	if (i == '7' && j == '8' && k == '9')
+	continue;
+	putchar(',');
+	putchar(' ');
+	}
+	}
+	}
+	}
+	putchar('\n');
+	return (0);
+}
