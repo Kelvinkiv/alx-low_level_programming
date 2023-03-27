a72fda5e8c9f333781f54207831089a3d990edbd
Kelvinkiv <kelvinkiv14@yahoo.com>
2023-03-27 07:10:03 -0700

    task 1- 2

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * reset_to_98 - Takes a pointer to an int and updates the value it points
+ * @n: An integer input pointer
+ * Return: nothing
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,17 @@
+include "main.h"
+#include <stdio.h>
+/**
+ * swap_int - swaps the values of two intergers
+ * @a: First int
+ * @b: Second int
+ * Return: 0 (Success)
+ */
+void swap_int(int *a, int *b)
+{
+	int i;
+
+	i = *a;
+	*a = *b;
+	*b = m;
+
+}
