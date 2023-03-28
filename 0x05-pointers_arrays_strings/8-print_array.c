a90b3d9e31a49106180ab9a0fceca254e2f7111d
Kelvinkiv <kelvinkiv14@yahoo.com>
2023-03-28 06:39:27 -0700

    101-keygen.c

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -1,21 +1,22 @@
 #include "main.h"
+#include <stdio.h>
 /*
  * print_array - Prints n elements of an array of integers
  * @a: array of integers
  * @n: number of elements of the array to be printed
- * Return: void
+ * Return: a and n inputs
  */
 void print_array(int *a, int n)
 {
 	int x;
 
-	for (x = 0; x < n; x++)
+	for (x = 0; x < (n - 1); x++)
 	{
-	printf("%d", a[x]);
-	if (x != (n - 1))
+	printf("%d, " , a[x]);
+	}
+	if (x == (n - 1))
 	{
-	printf(",");
+	printf("%d", a[n - 1])
 	}
-	}
-	printf('\n');
+	printf("\n");
 }
