f76154a2fe66a9446cbe9ba3198d260dc67c0246
Kelvinkiv <kelvinkiv14@yahoo.com>
2023-04-03 23:04:09 -0700

    100-set_string.c

--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -2,16 +2,16 @@
 /**
  * print_chessboard - Prints the chessboard
  * @a: Array
- * Return: 0 (Success)
+ * Return: void
  */
 void print_chessboard(char (*a)[8])
 {
-	int b, c;
+	int m, n;
 
-	for (a = 0; b < 8; b++)
+	for (m = 0; m < 8; m++)
 	{
-		for (c = 0; c < 8; c++)
-			_putchar(a[b][c]);
-		_putchar('\n');
+		for (n = 0; n < 8; n++)
+			_putchar(a[m][n]);
 	}
+	_putchar('\n');
 }
