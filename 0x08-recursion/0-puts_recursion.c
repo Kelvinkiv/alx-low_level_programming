2af759b7dca11c0481d8773c208db8572bb14d76
Kelvinkiv <kelvinkiv14@yahoo.com>
2023-04-05 07:45:45 -0700

    0-puts_recursion.c

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * _puts_recursion - Prints a string followed by a new line
+ * @s: input value
+ * Return: 0 (Success)
+ */
+void _puts_recursion(char *s)
+{
+	if (*s == '\0')
+	{
+		_putchar('\n');
+		_puts_recursion(s + 1);
+	}
+}
