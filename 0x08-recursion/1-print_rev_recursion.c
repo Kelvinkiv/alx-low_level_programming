cc629cae3c9fcf1d2934941ab840ca1b360e5dd9
Kelvinkiv <kelvinkiv14@yahoo.com>
2023-04-05 08:03:33 -0700

    1-print_rev_recursion.c

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * _print_rev_recursion - Prints a string in reverse
+ * @s: A pointer to a string
+ * Return: A reverse string
+ */
+void _print_rev_recursion(char *s)
+{
+	if (*s)
+	{
+		_putchar(*s);
+		_print_rev_recursion(s + 1);
+	}
+}
