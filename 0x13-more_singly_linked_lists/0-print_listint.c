f3069820b6c8740ae340ab448b78e7495d2eb343
Felix8134 <domnic8134@gmail.com>
2023-05-02 07:07:50 -0700

    0

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -7,14 +7,14 @@
  */
 size_t print_listint(const listint_t *h)
 {
-	size_t num = 0;
+	size_t a = 0;
 
 	while (h)
 	{
+		a++;
 		printf("%d\n", h->n);
-		num++;
 		h = h->next;
 	}
 
-	return (num);
+	return (a);
 }
